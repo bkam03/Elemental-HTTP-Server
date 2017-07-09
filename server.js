3f5de310b786210559d7cde92b3779217b1e54c6
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-07-08 17:00:22 -1000

    server.listen

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 /*jshint esversion: 6 */
 const http = require('http');
 const  fs = require('fs');
+const querystring = require('querystring');
 
 const server = http.createServer((request, response) => {
+
   console.log(request.method);
+  response.write('something', 'utf8', () =>{
+    response.end();
+    });
+
 });
 
-if(request.url === '/'){
-  fs.readFile('/index.html');
-}
+server.listen(8080, () =>{
+  console.log('server running');
+});
